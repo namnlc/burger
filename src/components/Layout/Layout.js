237d53bb08f91687fb5cf3e57c358967733d188a
namnlc <38275210+namnlc@users.noreply.github.com>
2020-08-27 11:20:09 +0700

    Side Drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,10 +3,12 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
 import ToolBar from '../Navigation/ToolBar/ToolBar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 const layout = (props) => (
     <Aux>
         <ToolBar />
+        <SideDrawer />
         <main className={classes.Content}>
             {props.children}
         </main>
